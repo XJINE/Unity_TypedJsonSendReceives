e26c4e9141fdac8187df8cf6bff3ae0114aab170
XJINE <xjine.andorelseif@gmail.com>
2025-04-23 20:07:50 +0900

    Remove targets field from Sender.

--- Assets/Packages/TypedJsonSendReceives/AbstractTypedJsonSender.cs
+++ Assets/Packages/TypedJsonSendReceives/AbstractTypedJsonSender.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using UnityEngine;
@@ -6,51 +7,26 @@ namespace TypedJsonSendReceives
 {
     public abstract class AbstractTypedJsonSender : MonoBehaviour
     {
-        #region Class
-
-        [System.Serializable] public class Target
-        {
-            public bool           enabled;
-            public IPEndPointInfo iPEndPoint;
-        }        
-
-        #endregion Class
-
         #region Field
 
-        [SerializeField] private Target[]       targets;
         [SerializeField] private TypedJsonEvent onSend;
 
         #endregion Field
 
-        #region Property
-
-        public Target[] Targets => targets;
-
-        #endregion Property
-
         #region Method
 
-        public void Send<T>(T instance)
+        public void Send<T>(T instance, IEnumerable<IPEndPoint> targets)
         {
-            var json       = GetJsonString(instance);
-            var typeString = typeof(T).ToString();
-            var jsonBytes  = Encoding.UTF8.GetBytes(typeString + ',' + json);
+            var json = GetJsonString(instance);
+            var data = Encoding.UTF8.GetBytes(typeof(T).ToString() + ',' + json);
 
             foreach (var target in targets)
             {
-                if (target.enabled)
-                {
-                    Send(target.iPEndPoint.IPEndPoint, jsonBytes);
-                }
+                Send(target, data);
             }
 
             onSend.Invoke(typeof(T), json);
         }
-
-        public void EnableAllTargets () { foreach (var target in targets) { target.enabled = true;            } }
-        public void DisableAllTargets() { foreach (var target in targets) { target.enabled = false;           } }
-        public void ToggleAllTargets () { foreach (var target in targets) { target.enabled = !target.enabled; } }
 
         protected abstract string GetJsonString<T>(T instance);
         protected abstract void   Send(IPEndPoint iPEndPoint, byte[] data);
